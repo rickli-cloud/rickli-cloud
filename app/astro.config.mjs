4e873ed0910455e9f907d53e8a2c85f97580c5e2
needForBeans <lucamrickli@gmail.com>
2024-05-24 15:00:34 +0200

    implement jamstack-cms

---
+++ app/astro.config.mjs
@@ -0,0 +1,52 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { jamstackCmsPlugin } from "@lucarickli/jamstack-cms-astro";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://www.rickli.cloud",
+  // base: "",
+  integrations: [
+    mdx(),
+    sitemap(),
+    jamstackCmsPlugin({
+      backend: {
+        force_auth: false,
+        git: {
+          repo: "rickli-cloud",
+          owner: "rickli-cloud",
+        },
+        auth: "return auth.githubOAuth('ef878b46674dd92c73ad', auth.urlParameter())",
+      },
+      collections: [
+        {
+          type: "dir",
+          name: "Blog",
+          path: "src/content/blog",
+          actions: {
+            create: true,
+            delete: true,
+          },
+          frontmatter: {
+            title: "z.string({})",
+            description: "z.string().optional()",
+            heroImage: "z.string().optional()",
+            pubDate: "z.date().default(() => new Date(Date.now()))",
+          },
+        },
+        {
+          type: "file",
+          name: "Pages",
+          actions: {
+            delete: true,
+          },
+          files: [
+            { path: "src/pages/datenschutz.md" },
+            { path: "src/pages/impressum.md" },
+          ],
+        },
+      ],
+    }),
+  ],
+});
