f3067b1f72b38eec9bb819519ba7566e95ab3889
lucarickli <luca@rickli.dev>
2025-01-06 00:07:42 +0100

    fix: admin config & site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://www.rickli.cloud",
   integrations: [
     mdx(),
     svelte(),
